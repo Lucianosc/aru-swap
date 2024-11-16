f614359f871efbf23dcaebb96ef6bd03c3dc1d0e
grmkris <kristjan.grm1@gmail.com>
2024-11-16 23:06:58 +0700

    Refactor getMetadata function to include cow_hook_dapp information

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -13,7 +13,22 @@ export const getMetadata = ({
   title: string;
   description: string;
   imageRelativePath?: string;
-}): Metadata => {
+}): Metadata & {
+  cow_hook_dapp: {
+    id: string;
+    name: string;
+    descriptionShort: string;
+    description: string;
+    version: string;
+    website: string;
+    image: string;
+    conditions: {
+      position: string;
+      smartContractWalletSupported: boolean;
+      supportedNetworks: number[];
+    };
+  };
+} => {
   const imageUrl = `${baseUrl}${imageRelativePath}`;
 
   return {
@@ -45,6 +60,20 @@ export const getMetadata = ({
     },
     icons: {
       icon: [{ url: "/aruswap.ico", sizes: "32x32", type: "image/png" }],
+    },
+    cow_hook_dapp: {
+      id: "06a2747d08f0026f47aebb91ac13172a318eb3f6116f742751e2d83cc61b8753",
+      name: "Your Hook Dapp",
+      descriptionShort: "Your Hook Dapp short description",
+      description: "Your Hook Dapp full description",
+      version: "0.0.1",
+      website: "https://your-cow-hook.dapp",
+      image: "http://your-cow-hook.dapp/logo.png",
+      conditions: {
+        position: "pre",
+        smartContractWalletSupported: false,
+        supportedNetworks: [1, 100, 42161],
+      },
     },
   };
 };
