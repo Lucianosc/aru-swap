97f0dffdcafe35d66f1cfa729ec2bdd3c1be34f2
Lucianosc <luchotandil90@gmail.com>
2024-11-17 03:10:12 +0700

    feat swap plugin page

--- packages/nextjs/app/cowswap/page.tsx
+++ packages/nextjs/app/cowswap/page.tsx
@@ -1,3 +1,13 @@
+import SwapSection from "~~/components/SwapSection";
+import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+
 export default function CowSwap() {
-  return <div>CowSwap</div>;
+  return (
+    <main className="p-4 max-w-lg flex flex-col gap-4 items-center justify-center mx-auto">
+      <div className="flex gap-2">
+        <RainbowKitCustomConnectButton />
+      </div>
+      <SwapSection />
+    </main>
+  );
 }
