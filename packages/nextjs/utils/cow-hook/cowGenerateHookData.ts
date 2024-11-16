f372a0e501ac0eb086d4fed8c6efedddcb731246
raigal-r <raquel.raigal@outlook.es>
2024-11-16 22:03:42 +0700

    merging to main

--- packages/nextjs/utils/cow-hook/cowGenerateHookData.ts
+++ packages/nextjs/utils/cow-hook/cowGenerateHookData.ts
@@ -2,6 +2,7 @@ import { encodeFunctionData } from 'viem';
 import abiUSDC from './abiUSDC';
 import {abiTokenMessenger} from './abiCCTP'
 
+
 // Approve USDC for spending by the CCTP contract
 const encodeApproveUSDC = (usdcAddress: string, cctpAddress: string, amount: string) => {   
 
@@ -16,14 +17,16 @@ const encodeApproveUSDC = (usdcAddress: string, cctpAddress: string, amount: str
  const encodeBurnUSDC = (usdcAddress: string, cctpAddress: string, amount: string, destinationDomain: number, destinationAddress: string) => {
   
   // Encode the destination address as bytes32
-  const encodedDestinationAddress = `0x${Buffer.from(destinationAddress.slice(2), 'hex').toString('hex').padStart(64, '0')}`;
-
+  //const encodedDestinationAddress = `0x${Buffer.from(destinationAddress.slice(2), 'hex').toString('hex').padStart(64, '0')}`;
+  //This is ok
+  const encodedMintRecipient = `0x${destinationAddress.slice(2).padStart(64, '0')}`; // Ensure the address is 40 characters long
+  console.log('encodedDestinationAddress',encodedMintRecipient);
   //await burnUSDC(provider, usdcAddress, cctpAddress, amount, destinationDomain, destinationAddress, signer);
-
+ 
   return encodeFunctionData({
     abi: abiTokenMessenger,
     functionName: 'depositForBurn',
-    args: [amount, destinationDomain, encodedDestinationAddress, usdcAddress],
+    args: [amount, destinationDomain, encodedMintRecipient, usdcAddress],
   });
  };
 
@@ -36,11 +39,12 @@ const generateCombinedCallData = (
   destinationAddress: string
 ) => {
   const approveData = encodeApproveUSDC(usdcAddress, cctpAddress, amount);
+  console.log('approveData: ', approveData)
   const burnData = encodeBurnUSDC(usdcAddress, cctpAddress, amount, destinationDomain, destinationAddress);
-
+    console.log("burn: ", burnData)
   // Combine both calls into a single calldata
   //return approveData + burnData.slice(2); // Remove the leading "0x" from burnData
-    return approveData + burnData.slice(2);
+    return  burnData;
 };
 
 export { generateCombinedCallData };
