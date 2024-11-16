848a247f8998b7ee2721f2954b98be3bd8ba62f5
grmkris <kristjan.grm1@gmail.com>
2024-11-16 22:48:26 +0700

    Add manifest file for Next.js App

--- packages/nextjs/utils/cow-hook/cowBuildData.ts
+++ packages/nextjs/utils/cow-hook/cowBuildData.ts
@@ -1,6 +1,5 @@
 // ~/utils/cow-hook/cowBuildData.ts
-
-import {generateCombinedCallData} from "./cowGenerateHookData";
+import { generateCombinedCallData } from "./cowGenerateHookData";
 
 const usdcAddress = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"; // Address of the USDC contract in Sepolia
 const cctpAddress = "0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5"; // Address of the CCTP's TokenMessenger contract in Sepolia
@@ -8,13 +7,7 @@ const amount = "1"; // Amount of USDC to approve and burn
 const destinationDomain = 3; // Destination domain (e.g., the chain ID)
 const destinationAddress = "0x0c558b655d388f7041bc4FbfbdF02AE1a605F19B"; // Change to msg.sender
 
-const calldata = generateCombinedCallData(
-  usdcAddress,
-  cctpAddress,
-  amount,
-  destinationDomain,
-  destinationAddress
-);
+const calldata = generateCombinedCallData(usdcAddress, cctpAddress, amount, destinationDomain, destinationAddress);
 
 console.log("Combined calldata:", calldata);
 
